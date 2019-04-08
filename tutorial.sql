95d51c3264573aca5f03a518c006c4a1d5a5d051
Pratik Bhoir <49150010+jyotibarai@users.noreply.github.com>
2019-04-08 12:11:34 +0530

    Jb : Updated the Code for Assignment 2

--- tutorial.sql
+++ tutorial.sql
@@ -5,5 +5,18 @@ SELECT
 from 
   Analytics_team.Assignment
 WHERE
-  Condition1 = Something
+  Condition1 = Something and 
   Condition2 = Something
+
+
+
+SELECT 
+  ColumnA,
+  ColumnB,
+  ColumnC,
+  ColumnD
+from 
+  Analytics_team.Assignment
+WHERE
+  Condition1 = Assignment1 and 
+  Condition2 = Assignment2
